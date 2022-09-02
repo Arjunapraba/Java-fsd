e4cb0ed91b6540495751263e7707a556daaa6c15
Arjun Kumar S <86823459+Arjunapraba@users.noreply.github.com>
2022-09-02 09:29:14 +0530

    Add files via upload

---
+++ Phase 3 -Assisted Projects/Default and Custom Event handling/Spring-Custom-Application-Events/src/main/java/com/turreta/springevent/ComTurretaSpringeventApplication.java
@@ -0,0 +1,22 @@
+package com.turreta.springevent;
+
+import com.turreta.springevent.events.AppEventA;
+import com.turreta.springevent.events.AppEventAPublisher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class ComTurretaSpringeventApplication  {
+
+	public static void main(String[] args) throws Exception{
+		ApplicationContext context = SpringApplication.run(ComTurretaSpringeventApplication.class, args);
+
+		AppEventAPublisher eventAPublisher = (AppEventAPublisher)context.getBean("appEventAPublisher");
+
+		eventAPublisher.publish(new AppEventA(new EventSource()));
+		TimeUnit.MINUTES.sleep(1);
+	}
+}
